ccdafa2ca2a4b438c79ab38302182315214aad7e
Edmund Dotsey <edmundped@gmail.com>
2021-12-31 21:14:22 +0000

    First Commit

---
+++ BankerRoulette.py
@@ -0,0 +1,9 @@
+import random
+names_string = input("Give me everybody's names, separated by a comma.\n")
+names = names_string.split(", ")
+counter = random.randint(0, len(names)-1)
+
+print(f"{names[counter]} will pay the bill today")
+
+##############you can use the choice function to select from a list ####################
+#print(f"{random.choice(names)} will pay the bill today")
